63a745684c0b0f5a4edce0f666771761b1ef2cbc
Harshit <bansalharshit31830@gmail.com>
2022-06-06 16:31:15 +0530

    Harshit

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -1,197 +1,116 @@
+let number_value = document.getElementsByClassName('numbers_value');
+let operator = document.getElementsByClassName('operator');
+let answer = document.getElementById('answer');
+let backspace = document.getElementById('backspace');
+let para = document.getElementById('para');
+let clear = document.getElementById('clear');
+let input = document.getElementById('input');
+let input2 = document.getElementById('input2');
+let comment = "Please enter the value";
 let a = "";
 let b = "";
 let status_a = 0;
 let status_b = 0;
-let input = document.getElementById('input');
-let input2 = document.getElementById('input2');
-let clear = document.getElementById('number_AC');
-let value = document.getElementsByClassName('numbers_value');
-let add = document.getElementById('number_add');
-let multiply = document.getElementById('number_multiply');
-let subtract = document.getElementById('number_subtract');
-let divide = document.getElementById('number_divide');
-let equal = document.getElementById('number_equal');
-let point = document.getElementById('number_point');
-let para = document.getElementById('para');
-let backspace = document.getElementById('backspace');
+let ans = 0;
 
-clear.addEventListener('click', () => {
-    input.value = "";
-    input2.value = "AC";
+function add_Data(index) {
+    if (status_a == 0) {
+        a += number_value[index].innerHTML;
+        input.value = a;
+    }
+    else {
+        b += number_value[index].innerHTML;
+        input.value = b;
+        status_b = 1;
+    }
+}
+
+function perform_expression() {
+    let expression = input2.value;
+    if (expression == '+')
+        ans = parseFloat(a) + parseFloat(b);
+
+    else if (expression == '-')
+        ans = parseFloat(a) - parseFloat(b);
+
+    else if (expression == '*')
+        ans = parseFloat(a) * parseFloat(b);
+
+    else if (expression == '/')
+        ans = parseFloat(a) / parseFloat(b);
+
+    input.value = ans;
+    para.innerHTML = `Answer = ${ans}`;
     a = "";
     b = "";
-});
+    status_a = 0;
+    status_b = 0;
+}
 
-add.addEventListener('click', () => {
-    if (status_a == 0) {
+function select_expression(index) {
+    input2.value = operator[index].innerHTML;
+    if (a == "" && ans != 0) {
+        a = ans;
         status_a = 1;
     }
-    else if (status_a == 1 && status_b == 0) {
-        para.innerHTML = "Please enter the values";
+    else if (a == "" && b == "") {
+        para.innerHTML = comment;
     }
-    else if (status_a != 0 && status_b != 0) {
-        let add;
-        if (typeof (a) != "string") {
-            add = a + parseFloat(b);
-        }
-        else {
-            add = parseFloat(a) + parseFloat(b);
-        }
-        input.value = add;
-        para.innerHTML = `Answer = ${add}`;
+    else if (a != "" && b == "") {
         status_a = 1;
-        status_b = 0;
-        a = add;
-        b = "";
     }
-    input2.value = "+";
-});
+    else if (a != "" && b != "") {
+        perform_expression();
+    }
+}
 
-divide.addEventListener('click', () => {
-    if (status_a == 0) {
-        status_a = 1;
-    }
-    else if (status_a == 1 && status_b == 0) {
-        para.innerHTML = "Please enter the values";
-    }
-    else if (status_a != 0 && status_b != 0) {
-        let add;
-        if (typeof (a) != "string") {
-            add = a / parseFloat(b);
-        }
-        else {
-            add = parseFloat(a) / parseFloat(b);
-        }
-        input.value = add;
-        para.innerHTML = `Answer = ${add}`;
-        status_a = 1;
-        status_b = 0;
-        a = add;
-        b = "";
-    }
-    input2.value = "/";
-});
-
-multiply.addEventListener('click', () => {
-    if (status_a == 0) {
-        status_a = 1;
-    }
-    else if (status_a == 1 && status_b == 0) {
-        para.innerHTML = "Please enter the values";
-    }
-    else if (status_a != 0 && status_b != 0) {
-        let add;
-        if (typeof (a) != "string") {
-            add = a * parseFloat(b);
-        }
-        else {
-            add = parseFloat(a) * parseFloat(b);
-        }
-        input.value = add;
-        para.innerHTML = `Answer = ${add}`;
-        status_a = 1;
-        status_b = 0;
-        a = add;
-        b = "";
-    }
-    input2.value = "*";
-});
-
-subtract.addEventListener('click', () => {
-    if (status_a == 0) {
-        status_a = 1;
-    }
-    else if (status_a == 1 && status_b == 0) {
-        para.innerHTML = "Please enter the values";
-    }
-    else if (status_a != 0 && status_b != 0) {
-        let add;
-        if (typeof (a) != "string") {
-            add = a - parseFloat(b);
-        }
-        else {
-            add = parseFloat(a) - parseFloat(b);
-        }
-        input.value = add;
-        para.innerHTML = `Answer = ${add}`;
-        status_a = 1;
-        status_b = 0;
-        a = add;
-        b = "";
-    }
-    input2.value = "-";
-});
-
-
-equal.addEventListener('click', () => {
+answer.addEventListener('click', () => {
     if (status_a == 0 || status_b == 0) {
-        para.innerHTML = "Please enter the values";
+        para.innerHTML = comment;
     }
-    else if (status_a != 0 && status_b != 0) {
-        let operation;
-        let sign = input2.value;
-        if (sign == '+')
-            operation = parseFloat(a) + parseFloat(b);
-        else if (sign == '*')
-            operation = parseFloat(a) * parseFloat(b);
-        else if (sign == '/')
-            operation = parseFloat(a) / parseFloat(b);
-        else if (sign == '-')
-            operation = parseFloat(a) - parseFloat(b);
-
-        input.value = operation;
+    else {
+        perform_expression();
         input2.value = "=";
-        para.innerHTML = `Answer = ${operation}`;
-        status_a = 0;
-        status_b = 0;
-        a = "";
-        b = "";
     }
-
 });
 
-value[0].addEventListener('click', () => {
-    if (status_a == 0) {
-        a += value[0].innerHTML;
+clear.addEventListener('click', () => {
+    a = "";
+    b = "";
+    status_a = 0;
+    stauts_b = 0;
+    input2.value = "AC";
+    input.value = "";
+    para.innerHTML = "";
+});
+
+backspace.addEventListener('click', () => {
+    if (a == "" && b == "") {
+        para.innerHTML = comment;
+    }
+    else if (a != "" && b == "") {
+        a = a.slice(0, a.length - 1);
         input.value = a;
     }
-    else {
-        b += value.innerHTML;
+    else if (b != "" && a != "") {
+        b = b.slice(0, b.length - 1)
         input.value = b;
-        status_b = 1;
     }
-});
+})
 
+number_value[0].addEventListener('click', () => { add_Data(0); });
+number_value[1].addEventListener('click', () => { add_Data(1); });
+number_value[2].addEventListener('click', () => { add_Data(2); });
+number_value[3].addEventListener('click', () => { add_Data(3); });
+number_value[4].addEventListener('click', () => { add_Data(4); });
+number_value[5].addEventListener('click', () => { add_Data(5); });
+number_value[6].addEventListener('click', () => { add_Data(6); });
+number_value[7].addEventListener('click', () => { add_Data(7); });
+number_value[8].addEventListener('click', () => { add_Data(8); });
+number_value[9].addEventListener('click', () => { add_Data(9); });
+number_value[10].addEventListener('click', () => { add_Data(10); });
 
-point.addEventListener('click', () => {
-    if (status_a == 0) {
-        a += ".";
-        input.value = a;
-    }
-    else {
-        b += ".";
-        input.value = b;
-        status_b = 1;
-    }
-});
-
-
-backspace.addEventListener('click', ()=>
-{
-    if(a == "" && b == "")
-    {
-        para.innerHTML = "Please enter the value";
-    }
-    if(a != "" && b == "")
-    {
-        console.log('First one')
-        a = a.slice(0, a.length-1);
-        input.value = a;        
-    }
-    else
-    {
-        console.log("second");
-        b = b.slice(0, b.length-1);
-        input.value = b;         
-    }
-});
+operator[0].addEventListener('click', () => { select_expression(0) });
+operator[1].addEventListener('click', () => { select_expression(1) });
+operator[2].addEventListener('click', () => { select_expression(2) });
+operator[3].addEventListener('click', () => { select_expression(3) });
