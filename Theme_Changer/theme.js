bdca309714223c214b8e4046d3f8797c89c55f2b
Harshit <bansalharshit31830@gmail.com>
2022-06-09 12:02:46 +0530

    TORCH and Converter project

--- Theme_Changer/theme.js
+++ Theme_Changer/theme.js
@@ -5,10 +5,12 @@ let para = document.getElementById("para");
 btn.addEventListener('click', () => {
     if (document.body.style.background == "" || document.body.style.background == "white") {
         document.body.style.background = 'black';
+        alert('Switching to Dark Mode');
         para.style.color = "white";
     }
     else {
         document.body.style.background = "white";
         para.style.color = "black";
+        alert('Switching to Light Mode');
     }
 })
